2b282e80b6a67983a85c1d0209983169328ae4b1
Yusuf.Q <yusufqing920@gmail.com>
2019-12-17 19:08:05 +0800

    fix entire module

--- calc.js
+++ calc.js
@@ -1,95 +1,174 @@
 $(document).ready(function(){
+  
+  // config variable
+  const compounding_per_year = 12;
+  const validateKeys = [0,1,2,3,4,5,6,7,8,9, "."];
+  
+  var doneTypingInterval = 500, 
+      property,
+      propertyValue,
+      deposit,
+      depositValue,
+      year,
+      years,
+      interest,
+      interestVal,
+      net,
+      netValue,
 
-    console.log("start")
-    function myFunction() {
-        var loan = $('#amount').val(),
-            month = $('#months').val(),
-            int = $('#interest').val(),
-            years = $('#years').val(),
-            down = $('#down').val(), 
-            amount = parseInt(loan),
-            months = parseInt(month),
-            down = parseInt(down),
-            annInterest = parseFloat(int),
-            monInt = annInterest / 1200,
-            calculation = ((monInt + (monInt / (Math.pow((1 + monInt), months) -1))) * (amount - (down || 0))).toFixed(2);
-      
-        document.getElementById("output").innerHTML = calculation;
+      monthlyPayment,
+      totalCost,
+      ltv,
+      totalMortgage,
+      typingTimer;
+
+  // call init fn
+  init();
+  
+
+  function init(){
+    autoFill();
+    $(".alert-warning").hide();
+  }
+
+  function autoFill(){
+    $("#compounding").val(compounding_per_year)
+  }
+  
+  function showAlert(){
+    $(".alert-warning").fadeIn();
+    setTimeout(() => {
+      $(".alert-warning").fadeOut();
+    }, 1000)
+  }
+  function validate(str){
+    
+    if ( !str || str.includes('-')) return false
+    else {
+      return str.split("").reduce((flag, e) => {
+        return flag && validateKeys.find(key => `${key}` === e) !== undefined
+      }, true)
     }
+  }
+
+  function formatResult(){
     
-    
-    $(function(){
-        var month = $(this).val(),
-          doneTypingInterval = 500,
-          months = parseInt(month),
-          typingTimer;
-    
-      $('#months').keyup(function(){
-          month = $(this).val();
-          months = parseInt(month);
+  }
+
+  function calcFn() {
+      property = $('#propertyValue').val();
+      deposit = $('#deposit').val();
+      net = $('#netValue').val();
+      year = $('#years').val();
+      month = $('#months').val();
+      interest = $('#interest').val();
+
+      propertyValue = parseFloat(property),
+      netValue = parseFloat(net),
+      depositValue = parseFloat(deposit),
+      months = parseFloat(month),
+      years = parseFloat(year),
+      interestVal = parseFloat(interest),
       
-          clearTimeout(typingTimer);
-          if (month) {
-              typingTimer = setTimeout(doneTyping, doneTypingInterval);
-          }
-      });
+      monthlyPayment = (propertyValue * interestVal / 100 / compounding_per_year).toFixed(2);
+      totalCost = (propertyValue * interestVal * years / 100).toFixed(2);
+      ltv = (totalCost / netValue * 100).toFixed(2);
+      totalMortgage = (propertyValue + parseFloat(totalCost)).toFixed(2);
+      
+      $("#monthlyPayment").text(monthlyPayment);
+      $("#totalCost").text(totalCost);
+      $("#ltv").text(ltv);
+      $("#totalMortgage").text(totalMortgage);
+  }
     
-      function doneTyping () {
-        $('#years').val(months/12);  
+  $(function(){ 
+    $('#interest').keyup(function(){
+      interest = $(this).val();
+      if (interest){
+        console.log("interest", interest)
+        if (interest.includes("-")) {
+          $('#interest').val("");
+          showAlert();
+        }
+        interestVal = parseFloat(interest)
+        if (interestVal > 100) {
+          $('#interest').val("");
+          showAlert();
+        }
       }
     })
+  })
+  
     
-    $(function(){
-        var month = $(this).val(),
-          doneTypingInterval = 500,
-          months = parseInt(month),
-          typingTimer;
-    
-      $('#months').keyup(function(){
-          month = $(this).val();
-          months = parseInt(month);
-      
+  $(function(){
+    $('#years').keyup(function(){
+        year = $(this).val();
+        if (validate(year)) {
+          years = parseFloat(year);
           clearTimeout(typingTimer);
-          if (month) {
-              typingTimer = setTimeout(doneTyping, doneTypingInterval);
-          }
-      });
-    
-      function doneTyping () {
-        $('#years').val(months/12);  
+          typingTimer = setTimeout(doneTyping, doneTypingInterval);
+        } else {
+          $(this).val("");
+          showAlert();
+        }
+    });
+  
+    function doneTyping () {
+      $('#months').val(Math.round(years * 12));  
+    }
+  })
+  
+  $(function(){
+  
+    $('#propertyValue').keyup(function(){
+        property = $(this).val();
+        deposit = $("#deposit").val();
+        if (!validate(property)) {
+          $(this).val("");
+          showAlert();
+          return;
+        } 
+        propertyValue = parseFloat(property);
+        depositValue = deposit?parseFloat(deposit):0;
+        clearTimeout(typingTimer);
+        typingTimer = setTimeout(doneTyping, doneTypingInterval);
+    });
+  
+    function doneTyping () {
+      $('#netValue').val(propertyValue - depositValue);  
+    }
+  })
+  
+  $(function(){
+    $('#deposit').keyup(function(){
+      deposit = $(this).val();
+      property = $("#propertyValue").val();
+      if (!validate(deposit)) {
+        $(this).val("");
+        showAlert();
+        return;
+      } 
+      depositValue = parseFloat(deposit);
+      propertyValue = property?parseFloat(property):0;
+      console.log("deposit",deposit, depositValue, !deposit ,propertyValue > depositValue)
+      clearTimeout(typingTimer);
+      if (!deposit || propertyValue > depositValue) {
+        typingTimer = setTimeout(doneTyping, doneTypingInterval);
       }
-    })
+    });
+  
+    function doneTyping () {
+      $('#netValue').val(propertyValue - depositValue);  
+    }
+  })
+  
+  
+  $("#calcForm").submit(function(e){
     
-    $(function(){
-        var year = $(this).val(),
-          doneTypingInterval = 500,
-          years = parseInt(year),
-          typingTimer;
+    e.preventDefault();
     
-      $('#years').keyup(function(){
-          year = $(this).val();
-          myears = parseInt(year);
-      
-          clearTimeout(typingTimer);
-          if (year) {
-              typingTimer = setTimeout(doneTyping, doneTypingInterval);
-          }
-      });
+    // $(this).reset();
+    calcFn();
     
-     
-    
-      function doneTyping () {
-        $('#months').val(year * 12);  
-      }
-    })
-    
-    $("#calc").click(function(){
-        console.log("this is it")
-        myFunction() 
-      })
-    
-    
-    
-    
-    
+  })
 })
